1f771a60551c609be496e238c7d3ed0cde731e7f
AsukaOurijya <azkaawliya06@gmail.com>
2025-09-08 00:17:22 +0700

    menambah link pws

--- el_football/settings.py
+++ el_football/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g^(s@c&oaegip!4=ai5n5jdmlp5-ox+24ta#nk&&e1xq%k!zz+
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1", "https://pbp.cs.ui.ac.id/muhammad.azka41/elfootball"]
 
 
 # Application definition
