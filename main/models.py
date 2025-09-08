ac5dd7567e4e3f7ad52c0bed2d7c4b126afd76dd
AsukaOurijya <azkaawliya06@gmail.com>
2025-09-08 14:49:57 +0700

    minor changes on models.py

--- main/models.py
+++ main/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 
 class product(models.Model):
+    CATEGORY_CHOICES = []
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
