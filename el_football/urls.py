a0dba8e28487ef73924e7df7b7aae0490458b007
AsukaOurijya <azkaawliya06@gmail.com>
2025-09-08 23:51:19 +0700

    Minor fixes on urls.py

--- el_football/urls.py
+++ el_football/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main.views import show_main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_main, name='show_main'),
-    #path('', include('main.urls'))
+    path('', include('main.urls'))
 ]
